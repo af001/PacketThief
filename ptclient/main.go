69b3eee19b8b36f9a26ba8dd6d83c281f723e965
Anton <af001@users.noreply.github.com>
2021-03-26 23:28:11 -0400

    Update main.go

--- ptclient/main.go
+++ ptclient/main.go
@@ -95,30 +95,6 @@ func buildBPF() string {
 	return filter
 }
 
-func showInterfaces() {
-	ifaces, err := net.Interfaces()
-	if err != nil {
-		fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
-		return
-	}
-	for _, i := range ifaces {
-		addrs, err := i.Addrs()
-		if err != nil {
-			fmt.Print(fmt.Errorf("localAddresses: %+v\n", err.Error()))
-			continue
-		}
-		for _, a := range addrs {
-			switch v := a.(type) {
-			case *net.IPAddr:
-				fmt.Printf("%v : %s (%s)\n", i.Name, v, v.IP.DefaultMask())
-
-			case *net.IPNet:
-				fmt.Printf("%v : %s [%v/%v]\n", i.Name, v, v.IP, v.Mask)
-			}
-		}
-	}
-}
-
 func startServer(receivers []*receiver) {
 
 	if *debug {
